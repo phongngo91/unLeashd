a85e026aecac75d3393eeff975f7589125d6c46b
Phong Ngo <phongngo91@gmail.com>
2020-02-03 18:50:56 -0500

    Created a test for users to save passwords on creation of a user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe User, type: :model do
             password: "super_secret_password"
         )
     end
+
+    describe "password encryption" do
+        it "does not save password to the database" do
+            User.create!(username: "mary_mack", password: "abcdef")
+            user = User.find_by(username: "mary_mack")
+            expect(user.password).to_not be("abcdef")
+        end
+    end
 end
