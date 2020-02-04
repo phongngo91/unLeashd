59ca0748bc7d1fc5019a6e6296279def787fa42a
Phong Ngo <phongngo91@gmail.com>
2020-02-03 19:41:10 -0500

    Renamed project to unLeashd

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module UnTappdClone
+module UnLeashd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.generators do |g|
