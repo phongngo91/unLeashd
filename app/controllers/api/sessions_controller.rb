372165dce8ee85597089d681729ec947b1b055c9
Phong Ngo <phongngo91@gmail.com>
2020-02-06 19:52:43 -0500

    Removed comments

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,18 +1,10 @@
 class Api::SessionsController < ApplicationController
-  
     def create
-      @user = User.find_by_credentials(
-        params[:username],
-        params[:password]
-      )
-  
+      @user = User.find_by_credentials(params[:username],params[:password])
       if @user 
-        # debugger
-  
         login!(@user)
         render :create
       else
-        #flash.now[:errors] = ["Invalid username or password"]
         render json: ["Invalid username or password"], status: 422
       end
     end
