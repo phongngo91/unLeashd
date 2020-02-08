e6f9b2cd27d32c88e0ee53d990bbf7e281a60e65
Phong Ngo <phongngo91@gmail.com>
2020-02-07 21:27:37 -0500

    Added hover effect to sign in button

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import LoginFormContainer from './login_form/login_form_container';
-import SignupFormContainer from './login_form/signup_form_container';
-import { AuthRoute, ProtectedRoute } from '../utils/route_utils';
-import HomeContainer from './home/home_container';
-import SplashContainer from './splash/spash_container';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import LoginFormContainer from "./login_form/login_form_container";
+import SignupFormContainer from "./login_form/signup_form_container";
+import { AuthRoute, ProtectedRoute } from "../utils/route_utils";
+import HomeContainer from "./home/home_container";
+import SplashContainer from "./splash/spash_container";
 
 const App = () => {
-    return (
-        <>
-        <Switch>
-            <AuthRoute exact path="/" component={SplashContainer} />
-            <AuthRoute path="/login" component={LoginFormContainer} />
-            <AuthRoute path="/signup" component={SignupFormContainer} />
-            <ProtectedRoute path="/home" component={HomeContainer} />
-        </Switch>
-        </>
-    );
+  return (
+    <>
+      <Switch>
+        <AuthRoute exact path="/" component={SplashContainer} />
+        <AuthRoute path="/login" component={LoginFormContainer} />
+        <AuthRoute path="/signup" component={SignupFormContainer} />
+        <ProtectedRoute path="/home" component={HomeContainer} />
+      </Switch>
+    </>
+  );
 };
 
 export default App;
