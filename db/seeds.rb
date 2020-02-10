dbd432a0e7232d6ec863b0bffec8c374a7225426
Phong Ngo <phongngo91@gmail.com>
2020-02-10 18:29:33 -0500

    Created and styled a functional demo user button for the login form

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@ ActiveRecord::Base.transaction do
     
     User.create!([
         {username: "phong", password: "hunter2", email: "phong@aa.com", first_name: "phong", last_name: "ngo"},
-        {username: "tommy", password: "hunter2", email: "tommy@aa.com", first_name: "tommy", last_name: "doe"}
+        {username: "tommy", password: "hunter2", email: "tommy@aa.com", first_name: "tommy", last_name: "doe"},
+        {username: "demo_user", password: "hunter2", email: "demo_user@aa.com", first_name: "userFirstName", last_name: "userLastName"}
     ])
     
 end
--- frontend/components/login/login_form.jsx
+++ frontend/components/login/login_form.jsx
@@ -9,6 +9,16 @@ class LoginForm extends React.Component {
       password: ""
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.loginDemo = this.loginDemo.bind(this);
+  }
+
+  loginDemo(e){
+    e.preventDefault();
+    const demoUser = {
+      username: "demo_user",
+      password: "hunter2"
+    };
+    this.props.processForm(demoUser).then(() => this.props.history.push("/home"));
   }
 
   update(property) {
@@ -30,17 +40,19 @@ class LoginForm extends React.Component {
     });
 
     let bottomEl = null;
-    if (this.props.formType === "Sign In"){
-        bottomEl = <>
-                    New around here? <Link to="/signup">Sign up!</Link>
-                </>
+    if (this.props.formType === "Sign In") {
+      bottomEl = (
+        <>
+          New around here? <Link to="/signup">Sign up!</Link>
+        </>
+      );
     }
 
     return (
-      <div className="login-content"
-      style={{ 
-        backgroundImage: 
-        `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)),
+      <div
+        className="login-content"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)),
         url(${window.dogsRunningURL})`
         }}
       >
@@ -48,8 +60,17 @@ class LoginForm extends React.Component {
           <div className="login-header">unleashd</div>
           <div className="login-subheader">pet gently</div>
           <div className="login-error">{errorsEl}</div>
-          <form className="login-form "onSubmit={this.handleSubmit}>
-            <div className="input-container">
+          <div className="demo-user-container">
+            <button onClick={this.loginDemo} className="demo-user-btn">Login with Demo User</button>
+          </div>
+          <div className="or">Or</div>
+          <form className="login-form " onSubmit={this.handleSubmit}>
+            <div
+              className="input-container"
+              style={{
+                backgroundImage: `url(${window.userIconURL})`
+              }}
+            >
               <input
                 className="input"
                 onChange={this.update("username")}
@@ -58,22 +79,27 @@ class LoginForm extends React.Component {
                 placeholder="Username"
               />
             </div>
-            <div className="input-container">
-              <input 
+            <div
+              className="input-container"
+              style={{
+                backgroundImage: `url(${window.passwordIconURL})`
+              }}
+            >
+              <input
                 className="input"
                 onChange={this.update("password")}
                 type="password"
                 value={this.state.password}
                 placeholder="Password"
               />
-            </div >
+            </div>
             <div className="login-submit-container">
-            <button className="login-submit-button">{this.props.formType}</button>
+              <button className="login-submit-button">
+                {this.props.formType}
+              </button>
             </div>
           </form>
-          <div className="login-container-bottom">
-            {bottomEl}
-          </div>
+          <div className="login-container-bottom">{bottomEl}</div>
         </div>
       </div>
     );
