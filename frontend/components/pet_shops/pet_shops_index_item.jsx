9161e3b736fbc1a6ff0b5affbb485f22975b36a6
Phong Ngo <phongngo91@gmail.com>
2020-02-11 21:56:32 -0500

    Added on click to the petshop's icon to link to the pet shop's show page

--- frontend/components/pet_shops/pet_shops_index_item.jsx
+++ frontend/components/pet_shops/pet_shops_index_item.jsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 const PetShopItem = ({ petShop }) => {
   return (
     <div key={petShop.id} className="pet-shop-item">
-      <img className="pet-shop-logo" src={petShop.image_url} alt="pet-shop" />
+      <Link to={`/petshops/${petShop.id}`}>
+        <img className="pet-shop-logo" src={petShop.image_url} alt="pet-shop" />
+      </Link>
       <div className="pet-shop-description">
         <div className="pet-shop-link">
           <Link to={`/petshops/${petShop.id}`}>{petShop.pet_shop_name}</Link>
