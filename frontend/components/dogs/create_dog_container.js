2d1a68b6d5613ced4303c8fbe9149e72521e95e4
Phong Ngo <phongngo91@gmail.com>
2020-02-13 23:03:27 -0500

    Small change to test heroku push to see if aws images persist

--- frontend/components/dogs/create_dog_container.js
+++ frontend/components/dogs/create_dog_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import CreateDog from './create_dog';
 import { createDog } from '../../actions/dog_actions';
 import { fetchPetShops } from '../../actions/pet_shops_actions';
-
+//
 const mapStateToProps = state => ({
   currentUser: state.entities.users[state.session.id],
   petShops: Object.values(state.entities.pet_shops)
