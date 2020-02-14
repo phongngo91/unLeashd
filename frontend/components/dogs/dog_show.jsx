157e3c49e71f3cf80e49d6700fa0b900d4c1ec1f
Phong Ngo <phongngo91@gmail.com>
2020-02-14 12:03:38 -0500

    Added div to the edit link so that it will have a line break

--- frontend/components/dogs/dog_show.jsx
+++ frontend/components/dogs/dog_show.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class DogShow extends React.Component {
   constructor(props) {
@@ -11,11 +11,10 @@ class DogShow extends React.Component {
   }
 
   render() {
-
     let editBtn = null;
-    if (this.props.loggedInUser && this.props.dog){
-      if (this.props.loggedInUser.id === this.props.dog.author_id){
-        editBtn = (<Link to={`/dogs/${this.props.dog.id}/edit`}>Edit Dog</Link>);
+    if (this.props.loggedInUser && this.props.dog) {
+      if (this.props.loggedInUser.id === this.props.dog.author_id) {
+        editBtn = <Link to={`/dogs/${this.props.dog.id}/edit`}>Edit Dog</Link>;
       }
     }
 
@@ -23,17 +22,13 @@ class DogShow extends React.Component {
       const { id, breed_name, image_url, description } = this.props.dog;
       return (
         <div className="petshop-show-container">
-            <div>
-              <img className="petshop-logo" src={image_url} alt="dog's image"/>
-              {editBtn}
-            </div>
-            <div className="petshop-show-description">
-              <div className="petshop-name">
-                {breed_name}
-              </div>
-              <div className="petshop-loc">
-                {description}
-              </div>
+          <div>
+            <img className="petshop-logo" src={image_url} alt="dog's image" />
+            <div>{editBtn}</div>
+          </div>
+          <div className="petshop-show-description">
+            <div className="petshop-name">{breed_name}</div>
+            <div className="petshop-loc">{description}</div>
           </div>
         </div>
       );
