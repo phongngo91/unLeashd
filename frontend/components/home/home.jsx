fc882ba3a729e1e0e251e0ae361020b040b8dfbe
Phong Ngo <phongngo91@gmail.com>
2020-02-19 13:22:06 -0500

    Refactored checkin cards in home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CheckinItemCard from "../checkin/checkin_item_card";
 
 class Home extends React.Component {
   constructor(props) {
@@ -22,78 +23,40 @@ class Home extends React.Component {
   }
 
   render() {
-    let checkins = null;
-    if (this.props.checkins.length > 0) {
-      checkins = this.props.checkins.reverse().map((checkin, idx) => {
-        let checkinImage = null;
-        let checkinBody = null;
-        if (checkin.image_url) {
-          checkinImage = (
-            <div className="checkin-img-container">
-              <img
-                className="checkin-img"
-                src={checkin.image_url}
-                alt="checkin-image"
-              />
-            </div>
-          );
-        }
-        if (checkin.checkin_body.length > 0) {
-          checkinBody = (
-            <div className="checkin-body-container">
-              <div className="checkin-body-text">{checkin.checkin_body}</div>
-            </div>
-          );
-        }
 
-        return (
-          <div key={idx} className="checkin-card">
-            <div className="checkin-user-info">
-              <span>
-                <span>
-                  <Link to={`users/${checkin.author_id}`} className="dog-name">
-                    {checkin.author.first_name} {checkin.author.last_name}
-                  </Link>
-                </span>{" "}
-                is petting a{" "}
-                <Link to={`dogs/${checkin.dog_breed_id}`} className="dog-name">
-                  {checkin.dog_breed.breed_name}
-                </Link>{" "}
-                at{" "}
-                <Link
-                  to={`/petshops/${checkin.pet_shop_id}`}
-                  className="dog-name"
-                >
-                  {checkin.pet_shop}
-                </Link>
-              </span>
-            </div>
-            {checkinBody}
-            <div>{checkinImage}</div>
-          </div>
-        );
-      });
+    const { checkins, currentUser, errors } = this.props;
+
+    let checkinCards = null;
+    if (checkins.length > 0) {
+      checkinCards = checkins
+        .reverse()
+        .map((checkin, idx) => (
+          <CheckinItemCard
+            checkin={checkin}
+            key={idx}
+            currentUser={currentUser}
+          />
+        ));
     }
 
     let moreButton = null;
-    if (this.props.errors.includes("No More Checkins")){
-      moreButton = (<div className="center-me">
-      <span>No More Checkins</span>
-      </div>)
+    if (errors.includes("No More Checkins")) {
+      moreButton = (
+        <div className="center-me">
+          <span>No More Checkins</span>
+        </div>
+      );
     } else {
       moreButton = (
         <div className="moreDog">
-        <button
-          onClick={this.getCheckins}
-          className="moreDogBtn remove-blue"
-        >
-          See More
-        </button>
-      </div>
-      )
+          <button onClick={this.getCheckins} className="moreDogBtn remove-blue">
+            See More
+          </button>
+        </div>
+      );
     }
 
-    const { first_name, last_name, username } = this.props.currentUser;
+    const { first_name, last_name, username } = currentUser;
     return (
       <div className="home-container">
         <div className="user-home-content">
@@ -101,7 +64,7 @@ class Home extends React.Component {
             <div className="global-checkins">
               <ul>
                 <h3 className="recent-feed-header">Recent Activities Feed</h3>
-                {checkins}
+                {checkinCards}
                 {moreButton}
               </ul>
             </div>
