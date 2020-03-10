1100ffe29741b4b397415f5121a3848b3cd1cfe9
Phong Ngo <phongngo91@gmail.com>
2020-03-10 10:01:02 -0400

    Add friendship index

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -1,2 +1,22 @@
 class Api::FriendshipsController < ApplicationController
+
+  def index
+    @user = User.find(params[:user_id])
+    render json: @user.friends
+  end
+
+  def create
+    @friendship = Friendship.new(friendship_params)
+  end
+
+  def delete
+
+  end
+
+  private 
+
+  def friendship_params
+    params.require(:friendship).permit(:user_id, :friend_id)
+  end
+
 end
