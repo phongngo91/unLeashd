24fe390dfd860ef588d0935156bff099b4acf770
Phong Ngo <phongngo91@gmail.com>
2020-03-09 21:40:59 -0400

    Add friendship controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: { format: 'json' } do
     resources :users, only: [:create, :show]
+    resources :friendships
     resources :dog_breeds, only: [:index, :show, :create, :update]
     resources :pet_shops, only: [:index, :show]
     resources :checkins, only: [:index, :create, :show, :destroy]
