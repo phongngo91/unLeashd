dc2d13762d2e5dc565c9ce2250b211f3185f93ad
Phong Ngo <phongngo91@gmail.com>
2020-03-13 10:08:24 -0400

    Add last name to search

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,11 @@
 class Api::UsersController < ApplicationController
 
   def index
-    @users = User.where(['lower(first_name) LIKE ?', "%#{params[:query].downcase}%"])
+    @users = User.where([
+      'lower(first_name) LIKE ? && lower(last_name) LIKE ?', 
+        "%#{params[:first_name].downcase}%", 
+        "%#{params[:last_name].downcase}%"
+    ])
     render :index
   end
 
