980c7f684d1511c4e86b3d0259fa8660e0d3cdc4
Phong Ngo <phongngo91@gmail.com>
2020-03-17 10:22:04 -0400

    Add AngelList icon

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin, faAngellist } from "@fortawesome/free-brands-svg-icons";
 
 class Spash extends React.Component {
   constructor(props) {
@@ -55,6 +55,15 @@ class Spash extends React.Component {
                   href="https://www.linkedin.com/in/phongngo91/"
                 >
                   <FontAwesomeIcon icon={faLinkedin} size="5x" color="white" />
+                </a>
+              </span>
+              <span>
+                <a
+                  target="_blank"
+                  title="AngelList"
+                  href="https://angel.co/phong-ngo"
+                >
+                  <FontAwesomeIcon icon={faAngellist} size="5x" color="white" />
                 </a>
               </span>
             </div>
