cd2139f33d772634157cee8f3954b211f7d2e527
Phong Ngo <phongngo91@gmail.com>
2020-03-17 11:40:52 -0400

    Add user search function

--- frontend/components/result/result_card.jsx
+++ frontend/components/result/result_card.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 
 const ResultCard = props => {
-  const { username, image_url } = props.user;
+  const { id, username, image_url, first_name, last_name } = props.user;
 
   return (
     <div className="result-container">
       <div className="result-wrapper">
         <div className="profile-img-wrapper">
-          <img src={image_url} alt="profile-image" className="img-prof"/>
+          <img src={image_url} alt="profile-image" className="img-prof" />
         </div>
         <div className="profile-info-wrapper">
-          <div>{username}</div>
+          <div>
+            <span>
+              <a href={`/#/users/${id}`}>
+                {username}
+              </a>
+            </span>
+          </div>
+          <div>
+            {first_name} {last_name}
+          </div>
         </div>
       </div>
     </div>
