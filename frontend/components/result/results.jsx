379d6c8d9b79867906fa5b57086694406553a8f1
Phong Ngo <phongngo91@gmail.com>
2020-03-26 06:18:58 -0400

    Style result page

--- frontend/components/result/results.jsx
+++ frontend/components/result/results.jsx
@@ -10,7 +10,12 @@ class SearchResult extends React.Component {
     const users = this.props.users;
     let result = null;
     if (users.length === 0) {
-      result = <div className="result-page">No Users Found</div>;
+      result =<div className="no-results-page">
+        <div className="no-results-text">No Users Found</div>
+        <br/>
+        <br/>
+        <div className="no-result-explain">Try searching for users by first name, or first name and last name</div>
+        </div>
     } else {
       const resultElements = users.map((user, idx) => {
         return <ResultCard key={idx} user={user} />;
