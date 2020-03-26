135760bd4768eee844cbab81071cc4bf748c12cd
Phong Ngo <phongngo91@gmail.com>
2020-03-26 06:43:04 -0400

    Hide footer from auth routes

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-const Footer = () => {
+const Footer = (props) => {
+  // Do not show footer on these pages
+  if (props.location.pathname === "/" ||
+  props.location.pathname === "/login" ||
+  props.location.pathname === "/signup"){
+    return null;
+  }
+
   return (
     <div className="footer-container">
       <a target="_blank" title="Portfolio" href="https://phongngo.net/">
